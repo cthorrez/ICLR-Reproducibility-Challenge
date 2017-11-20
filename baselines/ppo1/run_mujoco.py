d7cddb459135ce4495e46b69141141872f254a2c
thorrez <thorrez@umich.edu>
2017-11-20 14:10:47 -0500

    time remaining now a feature to be trained on

--- baselines/ppo1/run_mujoco.py
+++ baselines/ppo1/run_mujoco.py
@@ -5,15 +5,22 @@ import gym, logging
 from baselines import logger
 import roboschool
 import time_limit_envs
+from gym.wrappers import TimeLimit
+from gym import Wrapper
+
 
 def train(env_id, num_timesteps, seed):
     from baselines.ppo1 import mlp_policy, pposgd_simple
     U.make_session(num_cpu=1).__enter__()
     set_global_seeds(seed)
     env = gym.make(env_id)
+
+
     def policy_fn(name, ob_space, ac_space):
         return mlp_policy.MlpPolicy(name=name, ob_space=ob_space, ac_space=ac_space,
             hid_size=64, num_hid_layers=2)
+
+
     env = bench.Monitor(env, logger.get_dir())
     env.seed(seed)
     gym.logger.setLevel(logging.WARN)
--- time_limit_envs/limited_envs.py
+++ time_limit_envs/limited_envs.py
@@ -1,6 +1,9 @@
 from gym.envs.toy_text.frozen_lake import FrozenLakeEnv
 from roboschool.gym_mujoco_walkers import RoboschoolHopper
-
+from gym import Wrapper
+import time
+import numpy as np
+from gym import spaces
 
 
 # class FrozenLakeLargeShiftedIceEnv(FrozenLakeEnv):
@@ -22,9 +25,82 @@ from roboschool.gym_mujoco_walkers import RoboschoolHopper
 
 
 class RoboschoolHopperLimited(RoboschoolHopper):
+    def __init__(self):
+        super(RoboschoolHopperLimited, self).__init__()
+        self._max_episode_steps = 300
+        self._elapsed_steps = 0
+
+        low = self.observation_space.low
+        high = self.observation_space.high
+        low = np.concatenate((low, np.array([300])))
+        high = np.concatenate((high, np.array([300])))
+        self.observation_space = spaces.Box(low, high)
+
 
 
     def _step(self, action):
         observation, reward, done, info = super(RoboschoolHopperLimited, self)._step(action)
+        self._elapsed_steps += 1
+        time_remaining = self._max_episode_steps - self._elapsed_steps
+        time_remaining = np.array([time_remaining])
+        #print(time_remaining.shape)
+        observation = np.concatenate((observation, time_remaining))
         return observation, reward, done, info
+
+    def _reset(self):
+        self._elapsed_steps = 0
+        tmp = super(RoboschoolHopperLimited, self)._reset()
+        out = np.concatenate((tmp, np.array([0])))
+        return out
+
+
+
+
+
+
+
+
+
+
+class TimeLimit(Wrapper):
+    def __init__(self, env, max_episode_seconds=None, max_episode_steps=None):
+        super(TimeLimit, self).__init__(env)
+        self._max_episode_seconds = max_episode_seconds
+        self._max_episode_steps = max_episode_steps
+
+        self._elapsed_steps = 0
+        self._episode_started_at = None
+
+    @property
+    def _elapsed_seconds(self):
+        return time.time() - self._episode_started_at
+
+    def _past_limit(self):
+        """Return true if we are past our limit"""
+        if self._max_episode_steps is not None and self._max_episode_steps <= self._elapsed_steps:
+            logger.debug("Env has passed the step limit defined by TimeLimit.")
+            return True
+
+        if self._max_episode_seconds is not None and self._max_episode_seconds <= self._elapsed_seconds:
+            logger.debug("Env has passed the seconds limit defined by TimeLimit.")
+            return True
+
+        return False
+
+    def _step(self, action):
+        assert self._episode_started_at is not None, "Cannot call env.step() before calling reset()"
+        observation, reward, done, info = self.env.step(action)
+        self._elapsed_steps += 1
+
+        if self._past_limit():
+            if self.metadata.get('semantics.autoreset'):
+                _ = self.reset() # automatically reset the env
+            done = True 
+
+        return observation, reward, done, info
+
+    def _reset(self):
+        self._episode_started_at = time.time()
+        self._elapsed_steps = 0
+        return self.env.reset()
 
