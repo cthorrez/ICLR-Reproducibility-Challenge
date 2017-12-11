6c543327861fa84bf544226c1f26dfafdfc0bde2
thorrez <thorrez@umich.edu>
2017-12-10 21:53:06 -0500

    2-5

--- runReacherLimited.bash
+++ runReacherLimited.bash
@@ -1,2 +1,5 @@
-python3 -m baselines.ppo1.run_mujoco --env RoboschoolReacherLimited-v1 --seed 0
-python3 -m baselines.ppo1.run_mujoco --env RoboschoolReacherLimited-v1 --seed 1
+#python3 -m baselines.ppo1.run_mujoco --env RoboschoolReacherLimited-v1 --seed 0
+#python3 -m baselines.ppo1.run_mujoco --env RoboschoolReacherLimited-v1 --seed 1
+python3 -m baselines.ppo1.run_mujoco --env RoboschoolReacherLimited-v1 --seed 2
+python3 -m baselines.ppo1.run_mujoco --env RoboschoolReacherLimited-v1 --seed 3
+python3 -m baselines.ppo1.run_mujoco --env RoboschoolReacherLimited-v1 --seed 4
