2a9ee50af0d593462f7187206a6bdb91443fdf93
thorrez <thorrez@umich.edu>
2017-12-10 23:59:59 -0500

    35-40

--- runReacherLimited.bash
+++ runReacherLimited.bash
@@ -1,5 +1,13 @@
-#python3 -m baselines.ppo1.run_mujoco --env RoboschoolReacherLimited-v1 --seed 0
-#python3 -m baselines.ppo1.run_mujoco --env RoboschoolReacherLimited-v1 --seed 1
-python3 -m baselines.ppo1.run_mujoco --env RoboschoolReacherLimited-v1 --seed 2
-python3 -m baselines.ppo1.run_mujoco --env RoboschoolReacherLimited-v1 --seed 3
-python3 -m baselines.ppo1.run_mujoco --env RoboschoolReacherLimited-v1 --seed 4
+# python3 -m baselines.ppo1.run_mujoco --env RoboschoolReacherLimited-v1 --seed 0
+# python3 -m baselines.ppo1.run_mujoco --env RoboschoolReacherLimited-v1 --seed 1
+# python3 -m baselines.ppo1.run_mujoco --env RoboschoolReacherLimited-v1 --seed 2
+# python3 -m baselines.ppo1.run_mujoco --env RoboschoolReacherLimited-v1 --seed 3
+# python3 -m baselines.ppo1.run_mujoco --env RoboschoolReacherLimited-v1 --seed 4
+
+
+
+python3 -m baselines.ppo1.run_mujoco --env RoboschoolReacherLimited-v1 --seed 35
+python3 -m baselines.ppo1.run_mujoco --env RoboschoolReacherLimited-v1 --seed 36
+python3 -m baselines.ppo1.run_mujoco --env RoboschoolReacherLimited-v1 --seed 37
+python3 -m baselines.ppo1.run_mujoco --env RoboschoolReacherLimited-v1 --seed 38
+python3 -m baselines.ppo1.run_mujoco --env RoboschoolReacherLimited-v1 --seed 39
