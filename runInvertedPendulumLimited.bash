251100f55ff47d01fef2c81f52c01600a1ae39a9
thorrez <thorrez@umich.edu>
2017-12-10 19:36:47 -0500

    results

--- runInvertedPendulumLimited.bash
+++ runInvertedPendulumLimited.bash
@@ -1,2 +1,2 @@
-# python3 -m baselines.ppo1.run_mujoco --env RoboschoolInvertedPendulumLimited-v1 --seed 0
+python3 -m baselines.ppo1.run_mujoco --env RoboschoolInvertedPendulumLimited-v1 --seed 0
 python3 -m baselines.ppo1.run_mujoco --env RoboschoolInvertedPendulumLimited-v1 --seed 1
