d55157429d900593d435b926155e84dc7d3f0b3b
Riyu <riyu.banerjee@gmail.com>
2017-12-12 16:16:21 -0500

    10 - 14

--- runReacherLimited.bash
+++ runReacherLimited.bash
@@ -3,12 +3,23 @@
 # python3 -m baselines.ppo1.run_mujoco --env RoboschoolReacherLimited-v1 --seed 2
 # python3 -m baselines.ppo1.run_mujoco --env RoboschoolReacherLimited-v1 --seed 3
 # python3 -m baselines.ppo1.run_mujoco --env RoboschoolReacherLimited-v1 --seed 4
-python3 -m baselines.ppo1.run_mujoco --env RoboschoolReacherLimited-v1 --seed 5
-python3 -m baselines.ppo1.run_mujoco --env RoboschoolReacherLimited-v1 --seed 6
-python3 -m baselines.ppo1.run_mujoco --env RoboschoolReacherLimited-v1 --seed 7
-python3 -m baselines.ppo1.run_mujoco --env RoboschoolReacherLimited-v1 --seed 8
-python3 -m baselines.ppo1.run_mujoco --env RoboschoolReacherLimited-v1 --seed 9
+#python3 -m baselines.ppo1.run_mujoco --env RoboschoolReacherLimited-v1 --seed 5
+#python3 -m baselines.ppo1.run_mujoco --env RoboschoolReacherLimited-v1 --seed 6
+#python3 -m baselines.ppo1.run_mujoco --env RoboschoolReacherLimited-v1 --seed 7
+#python3 -m baselines.ppo1.run_mujoco --env RoboschoolReacherLimited-v1 --seed 8
+#python3 -m baselines.ppo1.run_mujoco --env RoboschoolReacherLimited-v1 --seed 9
 
+#python3 -m baselines.ppo1.run_mujoco --env RoboschoolReacherLimited-v1 --seed 10
+#python3 -m baselines.ppo1.run_mujoco --env RoboschoolReacherLimited-v1 --seed 11
+#python3 -m baselines.ppo1.run_mujoco --env RoboschoolReacherLimited-v1 --seed 12
+#python3 -m baselines.ppo1.run_mujoco --env RoboschoolReacherLimited-v1 --seed 13
+#python3 -m baselines.ppo1.run_mujoco --env RoboschoolReacherLimited-v1 --seed 14
+
+python3 -m baselines.ppo1.run_mujoco --env RoboschoolReacherLimited-v1 --seed 15
+python3 -m baselines.ppo1.run_mujoco --env RoboschoolReacherLimited-v1 --seed 16
+python3 -m baselines.ppo1.run_mujoco --env RoboschoolReacherLimited-v1 --seed 17
+python3 -m baselines.ppo1.run_mujoco --env RoboschoolReacherLimited-v1 --seed 18
+python3 -m baselines.ppo1.run_mujoco --env RoboschoolReacherLimited-v1 --seed 19
 
 
 
